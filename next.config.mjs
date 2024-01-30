5d327cfc546efc779f688a2c3cf811d1b6fa1d24
Aaryan Trivedi <itsaaryantrivedi@gmail.com>
2024-01-31 02:47:39 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,12 @@ module.exports = {
           hostname: 'b.thumbs.redditmedia.com',
           port: '',
           pathname: '/*',
-        },
+        },{
+        protocol: 'https',
+        hostname: 'i.redd.it',
+        port: '',
+        pathname: '/*',
+      },
         
       ],
     },
